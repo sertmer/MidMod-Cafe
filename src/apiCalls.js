5d6a6192a379ec04d759c9217cbb4d097a7c9834
sertmer <scottertmer@gmail.com>
2019-12-11 09:36:00 -0700

    Build and export get fetch

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,3 +1,9 @@
 export const getReservations = () => {
   return fetch('http://localhost:3001/api/v1/reservations	')
+    .then(response => {
+      if (!response.ok) {
+        throw Error('Error fetching reservations');
+      }
+      return response.json();
+    });
 }
