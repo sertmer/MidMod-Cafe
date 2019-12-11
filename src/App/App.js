40279c41f6b2362447c257945812a8a32527780e
sertmer <scottertmer@gmail.com>
2019-12-11 10:22:03 -0700

    pass addReservatino to Form

--- src/App/App.js
+++ src/App/App.js
@@ -6,7 +6,6 @@ import { getReservations } from '../apiCalls'
 class App extends Component {
   constructor() {
     super() 
-
     this.state = {
       reservations: []
     }
@@ -16,13 +15,19 @@ class App extends Component {
     getReservations()
       .then(reservations => this.setState({ reservations }))
   }
+
+  addReservation = (newReservation) => {
+    this.setState({reservations: [...this.state.reservations, newReservation]})
+  }
   
   render() {
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-        <Form />
+        <Form 
+          addReservation={this.addReservation}
+        />
         </div>
         <div className='resy-container'>
           <ReservationsContainer 
