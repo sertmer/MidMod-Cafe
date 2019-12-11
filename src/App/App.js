a8491fa2122b97f183ef1601aefc2faa2b305df3
sertmer <scottertmer@gmail.com>
2019-12-11 09:31:54 -0700

    import apiCalls and form

--- src/App/App.js
+++ src/App/App.js
@@ -1,23 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
-import ReservationsContainer from '../ReservationsContainer'
+import ReservationsContainer from '../ReservationsContainer/ReservationsContainer'
+import Form from '../Form/Form'
+import { getReservations } from '../apiCalls'
 class App extends Component {
   constructor() {
     super() 
 
     this.state = {
-      
+
     }
   }
+
+  componentDidMount() {
+    getReservations()
+  }
+  
   render() {
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-
+        <Form />
         </div>
         <div className='resy-container'>
-          
+          <ReservationsContainer />
         </div>
       </div>
     )
