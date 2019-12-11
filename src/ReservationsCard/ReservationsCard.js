855832be26bf1de42d98d6eca53a30462792edc1
sertmer <scottertmer@gmail.com>
2019-12-11 11:11:42 -0700

    Add cancel button

--- src/ReservationsCard/ReservationsCard.js
+++ src/ReservationsCard/ReservationsCard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './ReservationsCard.css'
 
 const ReservationsCard = ({id, name, date, time, number }) => {
   console.log(id)
@@ -8,6 +9,7 @@ const ReservationsCard = ({id, name, date, time, number }) => {
       <p>{date}</p>
       <p>{time}</p>
       <p>Number of Guests: {number}</p>
+      <button>cancel</button>
     </article>
   )
 }
