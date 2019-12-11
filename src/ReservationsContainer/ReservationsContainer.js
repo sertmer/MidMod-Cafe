911c0be493dd663aaca878241a2a209284af5959
sertmer <scottertmer@gmail.com>
2019-12-11 11:19:31 -0700

    import stylesheet

--- src/ReservationsContainer/ReservationsContainer.js
+++ src/ReservationsContainer/ReservationsContainer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './ReservationsContainer.css'
 import ReservationsCard from '../ReservationsCard/ReservationsCard'
 
 const ReservationsContainer = ({ reservations }) => {
