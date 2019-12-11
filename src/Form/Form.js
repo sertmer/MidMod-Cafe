7559a16281dc901d68e752e275d1a653e4f51c68
sertmer <scottertmer@gmail.com>
2019-12-11 09:24:14 -0700

    Build Initial Form

---
+++ src/Form/Form.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Form extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      title: '',
+      description: ''
+    }
+  }
+}
+
+export default Form
